32ea5740a05932dd24c4b01a0b66042fe6e03d71
jarico <3111825083@qq.com>
2025-03-01 17:11:50 +0800

    IMAGE_PR IMAGE_PR::ShowScalingImage(int targetValue, int x, int y, bool isHeightFixed) 	StretchBlt( 		hdcDst, 0, 0, targetWidth, targetHeight,  // 参数纠正

--- src/IMAGE_PR.cpp
+++ src/IMAGE_PR.cpp
@@ -81,7 +81,7 @@ void IMAGE_PR::ShowScalingImage(int targetValue, int x, int y, bool isHeightFixe
 	// 使用 StretchBlt 函数进行图像缩放
 	SetStretchBltMode(hdcDst, HALFTONE);  // 设置缩放模式为 HALFTONE，以获得更好的缩放质量
 	StretchBlt(
-		hdcDst, 0, 0, (int)Width_Height_Ratio * targetHeight, targetHeight,  // 目标区域（缩放后图像）
+		hdcDst, 0, 0, targetWidth, targetHeight,  // 目标区域（缩放后图像）
 		hdcSrc, 0, 0, OriginImage.getwidth(), OriginImage.getheight(),  // 源区域（原始图像）
 		SRCCOPY  // 复制模式
 	);
